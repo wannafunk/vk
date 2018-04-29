e4aec4150c010e02ada1ebd4e4ba284a0e9024ad
12345 <a12345@air-12345>
2018-04-29 16:09:30 +0300

    Initial Commit

---
+++ Vk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vk
+//
+//  Created by 12345 on 29.04.2018.
+//  Copyright © 2018 fanky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
